02b6b26f6b3f21ee4edcab9bf5b5fa7030916d53
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2015-09-06 20:31:11 +0100

    Example: Init SysTickTimer and Uart for testing.

--- examples/stm32f1_discovery/blink/main.cpp
+++ examples/stm32f1_discovery/blink/main.cpp
@@ -2,19 +2,25 @@
 
 using namespace Board;
 
-//using pll = Pll< InternalClock<>, MHz24 >;
-using pll = Pll< ExternalClock<MHz8>, 22000000 >;
-using systemClock = SystemClock< pll >;
-
+//using pll = Pll< InternalClock, MHz24 >;
+using pll = Pll< ExternalCrystal<MHz8>, 22000000 >;
+using systemClock = SystemClock< pll, AhbPrescaler::Div8 >;
 using clockOutput = ClockOutput< pll >;
+
+static_assert(systemClock::Fcpu == 2750000, "CPU Frequency is not 22/8 MHz!");
+static_assert(systemClock::Fcpu == pll::OutputFrequency / 8, "CPU Frequency is not 22/8 MHz!");
 
 MAIN_FUNCTION
 {
 	Board::initialize();
 	systemClock::enable();
+	xpcc::cortex::SysTickTimer::initialize<systemClock>();
 
 	clockOutput::enable();
 	GpioOutputA8::connect(clockOutput::Id);
+
+	GpioOutputA2::connect(Usart2::Tx);
+	Usart2::initialize<systemClock, 115200>(8);
 
 	LedBlue::set();
 
