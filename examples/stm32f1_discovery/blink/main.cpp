d1dae02028b43dd475b9796b1000f773ce5ade05
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2016-06-05 18:12:20 +0100

    Improve F1 Discovery example.

--- examples/stm32f1_discovery/blink/main.cpp
+++ examples/stm32f1_discovery/blink/main.cpp
@@ -1,29 +1,59 @@
 #include <xpcc/architecture/platform.hpp>
 
-// using namespace Board;
+// Choose a clock source.
+// Note: The internal clock does not have a user provided frequency,
+//       since its frequency is fixed!
+//using pllSource = InternalClock;
+using pllSource = ExternalCrystal<MHz8>;
+// Choose the PLL output frequency.
+// The pll values are automatically computed.
+using pll = Pll< pllSource, 22000000 >;
 
-//using pll = Pll< InternalClock, MHz24 >;
-using pll = Pll< ExternalCrystal<MHz8>, 22000000 >;
-using systemClock = SystemClock< pll, AhbPrescaler::Div8 >;
-using clockOutput = ClockOutput< pll >;
+// Choose clock input for system clock.
+//using systemSource = InternalClock;
+using systemSource = pll;
+// Choose AHB prescalers, all resulting frequencies are automatically computed.
+using systemClock = SystemClock< systemSource, AhbPrescaler::Div8 >;
+// System clock configuration can be asserted at compile time.
+static_assert(systemClock::Fcpu == 2750000,
+	"CPU Frequency is not 22/8 MHz!");
+static_assert(systemClock::Fcpu == pll::OutputFrequency / 8,
+	"CPU Frequency is not 1/8 of PLL output frequency!");
 
-static_assert(systemClock::Fcpu == 2750000, "CPU Frequency is not 22/8 MHz!");
-static_assert(systemClock::Fcpu == pll::OutputFrequency / 8, "CPU Frequency is not 22/8 MHz!");
+// Choose the low speed clock source.
+// using lsSource = LowSpeedInternalClock;
+using lsSource = LowSpeedExternalCrystal;
+// Choose clock input for RTC.
+using rtcClock = RealTimeClock< lsSource >;
+// Low speed clock configuration can also be asserted at compile time
+static_assert(rtcClock::Rtc == 32768,
+	"RTC frequency is not 32.768kHz!");
+
+// Choose clock output source.
+using clockOutput = ClockOutput< systemSource >;
 
 int
 main()
 {
-	Board::initialize();
+	// Enable the clocks and PLL.
 	systemClock::enable();
+	// Initialize the SysTickTimer with the chosen AHB frequency.
 	xpcc::cortex::SysTickTimer::initialize<systemClock>();
 
-	clockOutput::enable();
+	// Enable the low speed clocks.
+	rtcClock::enable();
+
+	// Connect Pin A8 to the clock output for measurement.
 	GpioOutputA8::connect(clockOutput::Id);
+	// Enable clock output with the chosen source clock.
+	clockOutput::enable();
+
 
 	GpioOutputA2::connect(Usart2::Tx);
 	Usart2::initialize<systemClock, 115200>(8);
 
-	Board::LedBlue::set();
+	Board::LedBlue::setOutput(xpcc::Gpio::High);
+	Board::LedGreen::setOutput(xpcc::Gpio::Low);
 
 	while (1)
 	{
