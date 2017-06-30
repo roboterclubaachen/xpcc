889595a093745e76366910b96d2d6650df02a17a
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2017-06-30 23:58:13 +0100

    Revert "[travis] Do not use caching."
    This reverts commit b9cbec70d4850dc4e16b319ee42d3c7f7326833d.

--- .travis.dep.sh
+++ .travis.dep.sh
@@ -1,20 +1,28 @@
-mkdir $HOME/toolchain
-# Download all tools
-echo "Downloading AVR toolchain..."
-(cd $HOME/toolchain && wget -q http://box.xpcc.io/avr-gcc.tar.bz2) &
-echo "Downloading Cortex-M toolchain..."
-(cd $HOME/toolchain && wget -q https://developer.arm.com/-/media/Files/downloads/gnu-rm/6-2017q2/gcc-arm-none-eabi-6-2017-q2-update-linux.tar.bz2 -O cortex-m.tar.bz2) &
-echo "Downloading libboost package..."
-(cd $HOME/toolchain && wget -q http://box.xpcc.io/boost.tar.bz2) &
+# Preparing the cache
+if [ ! -f "$HOME/cache/avr-gcc.tar.bz2" ]; then
+	echo "Downloading AVR toolchain..."
+	(cd $HOME/cache && wget -q http://box.xpcc.io/avr-gcc.tar.bz2) &
+fi
+if [ ! -f "$HOME/cache/cortex-m.tar.bz2" ]; then
+	echo "Downloading Cortex-M toolchain..."
+	(cd $HOME/cache && wget -q https://developer.arm.com/-/media/Files/downloads/gnu-rm/6-2017q2/gcc-arm-none-eabi-6-2017-q2-update-linux.tar.bz2 -O cortex-m.tar.bz2) &
+fi
+if [ ! -f "$HOME/cache/boost.tar.bz2" ]; then
+	echo "Downloading libboost package..."
+	(cd $HOME/cache && wget -q http://box.xpcc.io/boost.tar.bz2) &
+fi
+# wait for all downloads to finish
 wait
 echo "Downloads done."
 
 # unzip all toolchains
+mkdir $HOME/toolchain
 echo "Expanding AVR toolchain..."
-(cd $HOME/toolchain && tar -xjf avr-gcc.tar.bz2) &
+(cd $HOME/toolchain && tar -xjf $HOME/cache/avr-gcc.tar.bz2) &
 echo "Expanding Cortex-M toolchain..."
-(cd $HOME/toolchain && tar -xjf cortex-m.tar.bz2) &
+(cd $HOME/toolchain && tar -xjf $HOME/cache/cortex-m.tar.bz2) &
 echo "Expanding boost package..."
-(cd $HOME/toolchain && tar -xjf boost.tar.bz2) &
+(cd $HOME/toolchain && tar -xjf $HOME/cache/boost.tar.bz2) &
+# synchronize
 wait
 echo "Expanding done."
